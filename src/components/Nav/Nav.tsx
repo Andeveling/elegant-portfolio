c12f0d8f6b25586e51ea12d9ebe44aa5512f7d22
Andres Parra <andeveling@gmail.com>
2022-11-29 19:13:59 -0500

    add link cv

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -82,7 +82,7 @@ export default function Nav() {
           <Text h4 b>
             <Navbar.Link
               color={'warning'}
-              href='https://docs.google.com/document/d/1VwQAKk-Ei2Orp-374Cxvb8OxmEgBWeqVzoGvp8ECQAg/edit?usp=sharing'
+              href='https://docs.google.com/document/d/12w5OUXyvkhuErQT2KIqkYqNKXWGOu0fCu13TWffTB9Q/edit?usp=sharing'
               target='_blank'>
               Curriculum
             </Navbar.Link>
